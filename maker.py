471141e588ff154feaa574a66d4bd4e31d8342ec
mhockerm <mhockerm@purdue.edu>
2015-02-10 23:07:09 -0500

    more

--- maker.py
+++ maker.py
@@ -1,82 +1,132 @@
 
 # A replacement for stupid make files lol
 
-import math
 import os
 import subprocess
 import sys
 
-build_cmds = [
-    "lex lex.l",
-    "bison -y -d -g -t --verbose yacc.y",
-    "gcc y.tab.c lex.yy.c -o parser -lfl"
-]
+build_graph = {
+    "__start": "parser",
+
+    "parser": {
+        "__depends": ["y.tab.c", "lex.yy.c"],
+        "__do": "gcc y.tab.c lex.yy.c -o parser -lfl"
+    },
+
+    "y.tab.c": {
+        "__depends": [],
+        "__do": "bison -y -d -g -t --verbose yacc.y"
+    },
+
+    "lex.yy.c": {
+        "__depends": [],
+        "__do": "lex lex.l"
+    }
+}
 
 clean_cmd = "rm -f lex.yy.c y.tab.c y.tab.h y.dot y.output"
 
-test_cmd = "./parser {}"
+test_parameters = {
+    "__directory": "tests/",
+    "__command": "./parser {}",
+
+    "__pass_behavior": "no output",
+    "__exceptions": {
+        "provided_1": {
+            "__pass_behavior": "any output"
+        },
+        "provided_2": {
+            "__pass_behavior": "any output"
+        }
+    }
+}
 
 def build():
-    for cmd in build_cmds:
-        subprocess.call(cmd.split(" "))
+    build_level(build_graph["__start"])
 
-def clean():
-    subprocess.call(clean_cmd.split(" "))
+    def build_level(key):
+        for dependency in build_graph[key]["__depends"]:
+        build_level(dependency)
+        print "====> Building " + key
+        subprocess.call(build_graph[key]["__do"].split(" "))
 
-def test():
-    passed = 0
-    total = 0
-    for file in os.listdir("test"):
-        if ".expected" in file:
-            continue
-        cmd = test_cmd.format("test/" + file)
-        result = subprocess.check_output(cmd.split(" "), stderr=subprocess.STDOUT)
-        passing = open(file + ".expected", "r").read() == result
+        def build_with_make():
+            subprocess.call(["make", "clean"])
+            subprocess.call(["make"])
 
-        if passing:
-            print_test_pass(file)
-        else:
-            print_test_fail(file, result)
+            def clean():
+                subprocess.call(clean_cmd.split(" "))
 
-    print "\nPassed " + str(passed) + " / " + str(total) + " tests (" + str(math.ceil(100*float(passed)/total)) + ")."
+                def test():
+                    passed = 0
+                    total = 0
+                    for file in os.listdir(test_parameters["__directory"]):
+                        if os.path.isdir(test_parameters["__directory"] + file):
+                            continue
+                            total += 1
+                            if file in test_parameters["__exceptions"]:
+                            result = run_test(file, test_parameters["__exceptions"][file]["__pass_behavior"])
+                            else:
+                                result = run_test(file, test_parameters["__pass_behavior"])
+                                if result:
+                                    passed += 1
+                                    print "\nPassed " + str(passed) + " / " + str(total) + " tests (" + str(float(passed)/total) + ")."
 
-def print_test_pass(file):
-    print u"\033[0;32m\u2713" + "\033[0;00m\t" + file
+                                    def run_test(file, expect):
+                                        actual_name = test_parameters["__directory"] + file
+                                        output = subprocess.check_output(test_parameters["__command"].format(actual_name).split(" "), stderr=subprocess.STDOUT)
+                                        if expect == "no output":
+                                        if output == "":
+                                        print_test_pass(file)
+                                        return True
+                                        else:
+                                            print_test_fail(file, output)
+                                            return False
+                                            elif expect == "any output":
+                                            if output == "":
+                                            print_test_fail(file, output)
+                                            return False
+                                            else:
+                                                print_test_pass(file)
+                                                return True
 
-def print_test_fail(file, output):
-    print ""
-    print u"\033[0;31m\u2718" + "\t" + file
-    print output
-    print "\033[0;00m"
+                                                def print_test_pass(file):
+                                                    print u"\033[0;32m\u2713" + "\033[0;00m\t" + file
 
-def print_usage():
-    print "python maker.py [command]"
-    print "commands:"
-    print "\tbuild : runs the build() process as defined in the build graph"
-    print "\tclean : runs the command supplied in the clean command"
-    print "\ttest  : runs the testing framework as provided"
-    print "no command : does all three! build -> test -> clean!"
+                                                    def print_test_fail(file, output):
+                                                        print ""
+                                                        print u"\033[0;31m\u2718" + "\t" + file
+                                                        print output
+                                                        print "\033[0;00m"
 
-# Check arguments
+                                                        def print_usage():
+                                                            print "python maker.py [command]"
+                                                            print "commands:"
+                                                            print "\tbuild : runs the build() process as defined in the build graph"
+                                                            print "\tclean : runs the command supplied in the clean command"
+                                                            print "\ttest  : runs the testing framework as provided"
+                                                            print "no command : does all three! build -> test -> clean!"
 
-if len(sys.argv) > 2:
+                                                            # Check arguments
+
+                                                            if len(sys.argv) > 2:
     print_usage()
-    exit()
+exit()
 
-if len(sys.argv) == 1:
-    build()
+    if len(sys.argv) == 1:
+build()
     print "\nRunning Tests..."
-    test()
+test()
     print "\nCleaning..."
-    clean()
+clean()
 
-elif sys.argv[1] == "build":
-    build()
+    elif sys.argv[1] == "build":
+build()
 
-elif sys.argv[1] == "test":
-    test()
+    elif sys.argv[1] == "test":
+test()
 
-elif sys.argv[1] == "clean":
-    clean()
+    elif sys.argv[1] == "clean":
+clean()
 
 
