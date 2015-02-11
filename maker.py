9506e85abbc124676f1d45459aca2985458b05ef
mhockerm <mhockerm@purdue.edu>
2015-02-10 21:59:58 -0500

    stuff

--- maker.py
+++ maker.py
@@ -6,58 +6,19 @@ import os
 import subprocess
 import sys
 
-build_graph = {
-    "__start": "parser",
-    
-    "parser": {
-        "__depends": ["y.tab.c", "lex.yy.c"],
-        "__do": "gcc y.tab.c lex.yy.c -o parser -lfl"
-    },
-    
-    "y.tab.c": {
-        "__depends": [],
-        "__do": "bison -y -d -g -t --verbose yacc.y"
-    },
-
-    "lex.yy.c": {
-        "__depends": [],
-        "__do": "lex lex.l"
-    }
-}
+build_cmds = [
+    "lex lex.l",
+    "bison -y -d -g -t --verbose yacc.y",
+    "gcc y.tab.c lex.yy.c -o parser -lfl"
+]
 
 clean_cmd = "rm -f lex.yy.c y.tab.c y.tab.h y.dot y.output"
 
-test_parameters = {
-    "__directory": "tests/",
-    "__command": "./parser {}",
-    
-    "__pass_behavior": "no output",
-    "__exceptions": {
-        "provided_1": {
-            "__pass_behavior": "any output"
-        },
-        "provided_2": {
-            "__pass_behavior": "any output"
-        }
-    }
-}
+test_cmd = "./parser {}"
 
 def build():
-    build_level(build_graph["__start"], 1)
-
-def build_level(key, l):
-    for dependency in build_graph[key]["__depends"]:
-        build_level(dependency, l + 1)
-    arrow = ""
-    while l >= 0:
-        arrow += "="
-        l -= 1
-    print arrow + "> Building " + key
-    subprocess.call(build_graph[key]["__do"].split(" "))
-
-def build_with_make():
-    subprocess.call(["make", "clean"])
-    subprocess.call(["make"])
+    for cmd in build_cmds:
+        subprocess.call(cmd.split(" "))
 
 def clean():
     subprocess.call(clean_cmd.split(" "))
@@ -65,35 +26,19 @@ def clean():
 def test():
     passed = 0
     total = 0
-    for file in os.listdir(test_parameters["__directory"]):
-        if os.path.isdir(test_parameters["__directory"] + file):
+    for file in os.listdir("test"):
+        if ".expected" in file:
             continue
-        total += 1
-        if file in test_parameters["__exceptions"]:
-            result = run_test(file, test_parameters["__exceptions"][file]["__pass_behavior"])
-        else:
-            result = run_test(file, test_parameters["__pass_behavior"])
-        if result:
-            passed += 1
-    print "\nPassed " + str(passed) + " / " + str(total) + " tests (" + str(math.ceil(100*float(passed)/total)) + ")."
+        cmd = test_cmd.format("test/" + file)
+        result = subprocess.check_output(cmd.split(" "), stderr=subprocess.STDOUT)
+        passing = open(file + ".expected", "r").read() == result
 
-def run_test(file, expect):
-    actual_name = test_parameters["__directory"] + file
-    output = subprocess.check_output(test_parameters["__command"].format(actual_name).split(" "), stderr=subprocess.STDOUT)
-    if expect == "no output":
-        if output == "":
+        if passing:
             print_test_pass(file)
-            return True
         else:
-            print_test_fail(file, output)
-            return False
-    elif expect == "any output":
-        if output == "":
-            print_test_fail(file, output)
-            return False
-        else:
-            print_test_pass(file)
-            return True
+            print_test_fail(file, result)
+
+    print "\nPassed " + str(passed) + " / " + str(total) + " tests (" + str(math.ceil(100*float(passed)/total)) + ")."
 
 def print_test_pass(file):
     print u"\033[0;32m\u2713" + "\033[0;00m\t" + file
